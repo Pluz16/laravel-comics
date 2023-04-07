c924920162b58dbb7daceac8d09e982d54ec3452
Francesco Punta <puntafrancesco@gmail.com>
2023-04-07 17:19:53 +0200

    Array on routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comics', function () {
+    $comics = require_once(__DIR__ . '/../laravel-comics/comics.php');
+    return view('comics', ['comics' => $comics]);
+  });
+  
