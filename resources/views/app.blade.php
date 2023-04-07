4d5d4e8945f6dd4ba2c73713384338477124e2cb
Francesco Punta <puntafrancesco@gmail.com>
2023-04-07 17:18:25 +0200

    Partials

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,46 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('DComics')</title>
+    <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-<header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="#">LOGO</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Fumetti</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contatti</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
 
-    <main>
-        <div class="container">
-            @yield('content')
-        </div>
-    </main>
+    @include('partials.header')
+    
+    @yield('content')
+    
+    @include('partials.footer')
 
-    <footer>
-        <div class="container">
-            <hr>
-            <p>&copy; 2023 example.com</p>
-        </div>
-    </footer>
+    
 
     <script src="{{ mix('js/app.js') }}"></script>
     
